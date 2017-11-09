97ac1c424c0fcfdc5e2a230f5e04ea0f3ea35860
salaros <salaros@yandex.ru>
2017-11-09 17:32:56 +0300

    moving doc building script to docs/generate.sh

--- docs/generate.sh
+++ docs/generate.sh
@@ -2,14 +2,15 @@
 
 NAMESPACE="Salaros\\Vtiger\\VTWSCLib"
 NAMESPACE_2_PATH=$(echo ${NAMESPACE} | sed -e 's~\\~/~g')
-BUILD_PATH="./docs/_build"
-SPHPDOX_PATH="./vendor/sphpdox/sphpdox"
+BUILD_PATH="./_build"
+SPHPDOX_PATH="../vendor/sphpdox/sphpdox"
+SRC_PATH="../src"
 
 if [ -f ${SPHPDOX_PATH}/sphpdox.php ]; then
     [[ ! -d ${SPHPDOX_PATH}/ ]] && ln -sv $(pwd)/vendor/ ${SPHPDOX_PATH}/
-    ${SPHPDOX_PATH}/sphpdox.php process ${NAMESPACE} ./src -o ${BUILD_PATH}
+    php -f ${SPHPDOX_PATH}/sphpdox.php process ${NAMESPACE} ${SRC_PATH} -o ${BUILD_PATH}
 
-    mv -v ${BUILD_PATH}/$NAMESPACE_2_PATH/* ./docs/
+    mv -v ${BUILD_PATH}/$NAMESPACE_2_PATH/* ./
     rm -rf ${BUILD_PATH}/./*
 else
     echo 'Please install sphpdox via composer require --dev "sphpdox/sphpdox:dev-master"'
