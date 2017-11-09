842f91d31841fb5fde7e0b83aa9a93f89321e6ed
salaros <salaros@yandex.ru>
2017-11-09 16:58:40 +0300

    improving document generation-procedure

--- build-docs.sh
+++ build-docs.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 NAMESPACE="Salaros\\Vtiger\\VTWSCLib"
-NAMESPACE_PATH=$(echo ${NAMESPACE} | sed -e 's~\\~/~g')
-    echo $NAMESPACE_PATH
+NAMESPACE_2_PATH=$(echo ${NAMESPACE} | sed -e 's~\\~/~g')
+BUILD_PATH="./docs/_build"
+SPHPDOX_PATH="./vendor/sphpdox/sphpdox"
 
-if [ -f ./vendor/sphpdox/sphpdox/sphpdox.php ]; then
-    [[ ! -d ./vendor/sphpdox/sphpdox/ ]] && ln -sv $(pwd)/vendor/ ./vendor/sphpdox/sphpdox/
-    ./vendor/sphpdox/sphpdox/sphpdox.php process ${NAMESPACE} ./src -o ./docs/build
+if [ -f ${SPHPDOX_PATH}/sphpdox.php ]; then
+    [[ ! -d ${SPHPDOX_PATH}/ ]] && ln -sv $(pwd)/vendor/ ${SPHPDOX_PATH}/
+    ${SPHPDOX_PATH}/sphpdox.php process ${NAMESPACE} ./src -o ${BUILD_PATH}
 
-    mv -v ./docs/build/$NAMESPACE_PATH/* ./docs/
-    rm -rf ./docs/build/./*
+    mv -v ${BUILD_PATH}/$NAMESPACE_2_PATH/* ./docs/
+    rm -rf ${BUILD_PATH}/./*
 else
     echo 'Please install sphpdox via composer require --dev "sphpdox/sphpdox:dev-master"'
 fi
